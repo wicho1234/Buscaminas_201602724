acf94d3ab3ac8c29fade6fb8ede39ed9e0dc47ea
Luis <Luis@10.127.127.1>
2017-08-15 23:34:02 -0600

    holaa

--- src/buscaminas_201602724/menu.java
+++ src/buscaminas_201602724/menu.java
@@ -2,19 +2,27 @@
 package buscaminas_201602724;
 
 
+
+
 public class menu {
+   
+    
     public menu(){
         
-        while(true){
+            
+        
+        
         System.out.println("Luis Canel");
         System.out.println("201602724");
         System.out.println("IPC1 'A' Practica 1 ");
         System.out.println("BUSCAMINAS!");            
-        }
+        
 
 }    
-    public static void metodo(int a){
-        switch(a){
+    Integer opcion = 0;
+    public void metodo(int opc){
+        opcion = opc;
+        switch(opcion){
             case 1:
                 menu2 in = new menu2();
                 break;
